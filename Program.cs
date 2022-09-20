73ad0b08322f0bd5a2c3739e0d671e07bcdc123b
jenritzy <Jenritzy@outlook.com>
2022-09-20 13:37:05 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Switch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Switch";
+            int num = 3;
+            string day;
+
+            switch(num)
+            {
+                case 1: day = "Monday"; break;
+                case 2: day = "Tuesday"; break;
+                case 3: day = "Wednesday"; break;
+                case 4: day = "Thursday"; break;
+                case 5: day = "Friday"; break;
+                default: day = "Weekend Day"; break;
+            }
+            Console.WriteLine("Day " + num + ": " + day);
+            Console.ReadKey();
+        }
+    }
+}
